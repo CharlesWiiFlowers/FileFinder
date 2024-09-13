b1154dd48a1af2848fc1c212f1d47e341686d2a3
Carlos563429 <carlitosfg.5051@gmail.com>
2024-09-13 12:12:17 -0600

    [🦀] Fixed library mistakes!!!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-struct File{
-    name: String,
-    rootSearch: String
-}
+use clap::Parser;
 
+// This is a macro
+#[derive(Parser)]
+#[command(name = "search")]
+#[command(about = "Search a specific file in your system", long_about = "This request a name and verify
+ your files on your system.")]
+struct Cli{
+    #[arg(short, long, default_value = "World")]
+    path: String
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Hello, {}!", args.path);
 }
