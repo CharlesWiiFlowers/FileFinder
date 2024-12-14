a20f2af91dcd372582387f40ac2862bc9d53ddf3
Carlos563429 <carlitosfg.5051@gmail.com>
2024-12-14 06:04:14 -0600

    [⛵] Add Subcommands support
    (Don't work)

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,42 @@
-use std::{fs, io::Write, path::Path, sync::mpsc, thread, time::{Duration, Instant}};
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use std::{
+    fs,
+    io::Write,
+    path::Path,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
 
 // This is a macro
 #[derive(Parser)]
-#[command(name = "search")]
-#[command(
-    about = "Search a specific file in your system",
-    long_about = "This request a name and verify
- your files on your system."
-)]
+#[command(about = "A toolbox of utilities", long_about = "")]
 struct Cli {
-    #[arg(short, long)]
-    filename: String,
+    #[clap(subcommand)]
+    command: Commands,
+}
 
-    #[arg(short, long, default_value = "C://")]
-    root: String,
+#[derive(Subcommand)]
+enum Commands {
+    Find {
+        #[clap(short, long)]
+        filename: String,
+    
+        #[clap(short, long, default_value = "C://")]
+        root: String,
+    }
 }
 
 fn main() {
-
     // Time elapsed
     let now = Instant::now();
 
     // Spinner section
     // This gonna send a stop signal
     // mpsc = multiple producer, single consumer
-    let(tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
     let spinner_handle = thread::spawn(move || {
-
         // Vector with the future emoji spinner
         let spinner_chars: Vec<&str> = vec!["🌕", "🌖", "🌗", "🌘", "🌑", "🌒", "🌓", "🌔"];
         let mut i = 0;
@@ -41,13 +49,16 @@ fn main() {
 
             // So print the currecnt char of the spinner
             // the index will be the result of divide de cycle number upper the spinner_chars lenght
-            print!("\r{} Loading... {}", spinner_chars[i % spinner_chars.len()], spinner_chars[i % spinner_chars.len()]);
-            i+=1;
+            print!(
+                "\r{} Loading... {}",
+                spinner_chars[i % spinner_chars.len()],
+                spinner_chars[i % spinner_chars.len()]
+            );
+            i += 1;
 
             std::io::stdout().flush().unwrap();
             thread::sleep(Duration::from_millis(200));
         }
-        
     });
 
     // Main Section
@@ -61,19 +72,24 @@ fn main() {
             for path in paths {
                 println!("{path}");
             }
-            
-            println!("Work finished!! 🐾\nTranscurred time: {} ms", now.elapsed().as_millis());
+
+            println!(
+                "Work finished!! 🐾\nTranscurred time: {} ms",
+                now.elapsed().as_millis()
+            );
 
             //Let it finish
             let _ = spinner_handle.join();
         }
         None => {
             tx.send(()).unwrap();
-            println!("\nNo matches found!! 🚀\nTranscurred time: {} ms", now.elapsed().as_millis());
+            println!(
+                "\nNo matches found!! 🚀\nTranscurred time: {} ms",
+                now.elapsed().as_millis()
+            );
             let _ = spinner_handle.join();
         }
     }
-
 }
 
 // Search a name in a dir
@@ -87,12 +103,10 @@ fn search(dir: &str, filename: &str) -> Option<Vec<String>> {
         for entry in entries {
             // If entry returns Ok
             if let Ok(entry) = entry {
-
                 let entry_path = entry.path();
-                
+
                 if entry_path.is_dir() {
-                    if let Some(mut subdir_matches) =
-                        search(entry_path.to_str().unwrap(), filename)
+                    if let Some(mut subdir_matches) = search(entry_path.to_str().unwrap(), filename)
                     {
                         matching_files.append(&mut subdir_matches);
                     }
